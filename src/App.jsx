0efdc0ac4418d43f4090ed881de1f14823066848
Qutred <shloser.andriy@gmail.com>
2024-01-15 16:32:03 +0200

    added starter files

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import Homepage from './pages/Homepage'
+import PageNotFound from './pages/PageNotFound'
+import './index.css'
+import AppLayout from './pages/AppLayout'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='app' element={<AppLayout />}>
+          <Route index element={<p>List</p>} />
+          <Route path='cities' element={<p>List of cities</p>} />
+          <Route path='countries' element={<p>Countries</p>} />
+          <Route path='forms' element={<p>Forms</p>} />
+        </Route>
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
