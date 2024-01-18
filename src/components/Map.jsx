2f7cb923bb3bf8157ff9ae08ac19feda7064a6e4
Qutred <shloser.andriy@gmail.com>
2024-01-18 13:36:11 +0200

    added simpla leaflet map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,33 @@
+import {useState} from 'react'
 import {useNavigate, useSearchParams} from 'react-router-dom'
 import styles from './Map.module.css'
+import {MapContainer, Marker, TileLayer, Popup} from 'react-leaflet'
 
 const Map = () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const navigate = useNavigate()
+
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09])
   const lat = searchParams.get('lat')
   const lng = searchParams.get('lng')
   console.log(lat, lng)
   return (
     <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      Map
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={`${styles.map}`}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
